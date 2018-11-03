c7fdfd5bf122547ddb0d5d2c96824b990d9f91c0
jprokopowicz <jacekjoeprokopowicz@gmail.com>
2018-11-03 18:17:47 +0100

    Reader methods. Fix Position.

--- src/com/lexer/Position.java
+++ src/com/lexer/Position.java
@@ -1,44 +1,17 @@
 package com.lexer;
 
-import javax.naming.LimitExceededException;
-
 public class Position {
-    private int line;
-    private int sign;
+    public int line;
+    public int sign;
 
     public Position(){
-        line = 0;
-        sign = 0;
+        line = 1;
+        sign = 1;
     }
 
-    public Position(int line, int sign) throws LimitExceededException{
-        setPosition(line,sign);
-    }
-    public int getLine() {
-        return this.line;
-    }
-
-    public void setLine(int line) throws LimitExceededException {
-        if (line < 0) {
-            throw new LimitExceededException("Position is positive");
-        }
+    public Position(int line, int sign){
         this.line = line;
-    }
-
-    public int getSign() {
-        return sign;
-    }
-
-    public void setSign(int sign) throws LimitExceededException {
-        if (sign < 0) {
-            throw new LimitExceededException("Position is positive");
-        }
         this.sign = sign;
-    }
-
-    public void setPosition(int line, int sign) throws LimitExceededException{
-        setLine(line);
-        setSign(sign);
     }
 
     public void print() {
--- src/com/lexer/Reader.java
+++ src/com/lexer/Reader.java
@@ -1,4 +1,39 @@
 package com.lexer;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Reader {
+    private FileInputStream fileInputStream;
+    private int readBytes;
+    private Position position;
+
+    public Reader(FileInputStream fileInputStream) {
+        this.fileInputStream = fileInputStream;
+        readBytes = 0;
+        position = new Position();
+    }
+
+    public char get() throws IOException{
+        char sign = (char)fileInputStream.read();
+        readBytes++;
+        position.sign++;
+        if(sign=='\n'){
+            position.sign = 1;
+            position.line++;
+        }
+        return sign;
+    }
+
+    public char peek() throws IOException {
+        char sign = (char)fileInputStream.read();
+        fileInputStream.reset();
+        if(fileInputStream.available() < fileInputStream.skip(readBytes))
+            throw new IOException();
+        return sign;
+    }
+
+    public boolean eof() throws IOException{
+        return fileInputStream.available() == 0;
+    }
 }
