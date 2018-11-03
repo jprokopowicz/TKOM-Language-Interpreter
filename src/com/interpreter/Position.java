bbcac7f4d5fc47166ee7a333d1d9fbde4d1ca312
jprokopowicz <jacekjoeprokopowicz@gmail.com>
2018-11-03 01:42:57 +0100

    Add data classes, Token and Position.

--- src/com/interpreter/Position.java
+++ src/com/interpreter/Position.java
@@ -1,4 +1,47 @@
 package com.interpreter;
 
+import javax.naming.LimitExceededException;
+
 public class Position {
+    private int line;
+    private int sign;
+
+    Position(){
+        line = 0;
+        sign = 0;
+    }
+
+    Position(int line, int sign) throws LimitExceededException{
+        setPosition(line,sign);
+    }
+    public int getLine() {
+        return this.line;
+    }
+
+    public void setLine(int line) throws LimitExceededException {
+        if (line < 0) {
+            throw new LimitExceededException("Position is positive");
+        }
+        this.line = line;
+    }
+
+    public int getSign() {
+        return sign;
+    }
+
+    public void setSign(int sign) throws LimitExceededException {
+        if (sign < 0) {
+            throw new LimitExceededException("Position is positive");
+        }
+        this.sign = sign;
+    }
+
+    public void setPosition(int line, int sign) throws LimitExceededException{
+        setLine(line);
+        setSign(sign);
+    }
+
+    void print() {
+        System.out.println("line: " + line + " sing: " + sign);
+    }
 }
--- src/com/interpreter/Token.java
+++ src/com/interpreter/Token.java
@@ -1,4 +1,54 @@
 package com.interpreter;
 
 public class Token {
+    public enum Type {
+        var_,
+        frantion_,
+        string_,
+        bool_,
+        true_,
+        false_,
+        void_,
+        main_,
+        function_,
+        return_,
+        loop_,
+        if_,
+        else_,
+        write_,
+        read,
+        comma_,
+        semicon_,
+        assign_,
+        open_bracket_,
+        clese_bracket,
+        open_scope_,
+        close_scope_,
+        plus_,
+        minus_,
+        star_,
+        slash_,
+        modulo_,
+        or_,
+        and_,
+        not_,
+        greater_,
+        greater_equal_,
+        lesser_,
+        lesser_equal_,
+        equal_,
+        not_equal_,
+        comment_,
+        identifier_,
+        number_expression_,
+        string_expression_,
+        invalide_,
+        end_of_file
+    }
+
+    private Type type = Type.invalide_;
+    private String value = "";
+    private Position position;
+
+
 }
