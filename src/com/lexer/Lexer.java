cdadbbff9576e2269ae8c0e6e35c53cdf03433ac
jprokopowicz <jacekjoeprokopowicz@gmail.com>
2018-11-06 20:39:17 +0100

    Partly lexer implementation.

--- src/com/lexer/Lexer.java
+++ src/com/lexer/Lexer.java
@@ -12,25 +12,84 @@ public class Lexer {
         token = new Token(Token.Type.invalid_, "",  new Position());
     }
 
+    /**
+     * Reads next token from the stream.
+     * @return Read token.
+     */
     public Token readNextToken(){
         //TODO: read nets token
-        try{
-            while(Character.isWhitespace(streamReader.lookUpByte())){
-                streamReader.readByte();
-                if(streamReader.endOfFile()){
-                    token = new Token(Token.Type.end_of_file_,"", streamReader.getPosition());
-                    return token;
-                }
-            }
-
-
-        } catch (IOException e) {
-            token = new Token(Token.Type.invalid_, "",  new Position());
+        if(!skipWhiteSigns()){
+            return token;
         }
+
+        Position tokenPosition = streamReader.getPosition();
+
         return token;
     }
 
+    /**
+     * Token getter
+     * @return last detected token.
+     */
     public Token getToken(){
         return token;
+    }
+
+    /**
+     * Skips white signs in the input stream.
+     * If it reached end of stream sets token.type to endOfFile_.
+     * If there were any IO Exceptions during skipping it sets token.type to invalid_.
+     * @return Are there any signs left to read.
+     */
+    private boolean skipWhiteSigns(){
+        try{
+            while(!streamReader.endOfFile()&&Character.isWhitespace(streamReader.lookUpByte())){
+                streamReader.readByte();
+            }
+            if(streamReader.endOfFile()){
+                token = new Token(Token.Type.end_of_file_,"", streamReader.getPosition());
+                return false;
+            }
+        } catch (IOException e) {
+            token = new Token(Token.Type.invalid_, "", new Position());
+            return false;
+        }
+        return true;
+    }
+
+    private void defineKeywordOrIdentifier(){
+
+    }
+
+    private void defineNumericLiteral(){
+
+    }
+
+    private void defineSpecialSignOrString(){
+
+    }
+
+    private void defineStringExpression(StringBuffer buffer){
+
+    }
+
+    private void defineCommentExpression(StringBuffer buffer){
+
+    }
+
+    /**
+     * Reads token value to the end and stores it in the buffer.
+     * @param buffer StringBuffer for storing value.
+     */
+    private void continueToTokenEnd(StringBuffer buffer){
+        try {
+            if(streamReader.endOfFile()){
+                return;
+            }
+            for (char sign = streamReader.lookUpByte(); Character.isAlphabetic(sign) || Character.isDigit(sign) || sign == '_'; sign = streamReader.lookUpByte())
+                buffer.append(streamReader.readByte());
+        } catch (IOException exc) {
+            System.out.println(exc.getMessage());
+        }
     }
 }
