88e8c5e1301847055655943e0f20c24e0169e6af
jprokopowicz <jacekjoeprokopowicz@gmail.com>
2018-12-01 02:26:14 +0100

    Example code for testing and main reading the containing file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import com.lexer.Lexer;
 import com.lexer.StreamReader;
+import com.lexer.Token;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -12,13 +13,22 @@ public class Main {
                 FileInputStream inputStream = new FileInputStream(args[0]);
                 Lexer lexer = new Lexer(new StreamReader(inputStream));
                 //Lexer operations
+                System.out.println("start");
+                while(lexer.getToken().getType() != Token.Type.end_of_file_){
+                    Token token = lexer.readNextToken();
+                    System.out.println(token.getPosition().sign + "\t" + token.getPosition().line + "\t" + Token.tokenNames.get(token.getType()) + ":\t\t" +  token.getValue());
+                }
+                System.out.println("end");
             } catch (FileNotFoundException exc){
                 System.out.println("No such file or invalid argument: " + exc.getMessage());
             } catch (Exception exc){
                 System.out.println("Other exception: "+ exc.getMessage());
+                exc.printStackTrace();
             }
-        } else {
+        } else if(args.length == 0){
             System.out.println("No given file");
+        } else {
+            System.out.println("Too many arguments");
         }
     }
 }
