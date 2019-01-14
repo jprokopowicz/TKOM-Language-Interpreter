bbf008eaad3de4c80ded325041468984cf2e1cc6
jprokopowicz <jacekjoeprokopowicz@gmail.com>
2019-01-14 23:24:36 +0100

    Parser main tests

---
+++ src/com/LexerMain.java
@@ -0,0 +1,35 @@
+package com;
+
+import com.byteReader.StreamReader;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class LexerMain {
+
+    public static void main(String[] args) {
+        if(args.length == 1){
+            try {
+                FileInputStream inputStream = new FileInputStream(args[0]);
+                Lexer lexer = new Lexer(new StreamReader(inputStream));
+                lexer.setIgnoreComment(false);
+                //Lexer operations
+                System.out.println("start");
+                while(lexer.getToken().getType() != Token.Type.end_of_bytes_){
+                    Token token = lexer.readNextToken();
+                    System.out.println(token.getPosition().sign + "\t" + token.getPosition().line + "\t" + Token.tokenNames.get(token.getType()) + ":\t\t" +  token.getValue());
+                }
+                System.out.println("end");
+            } catch (FileNotFoundException exc){
+                System.out.println("No such file or invalid argument: " + exc.getMessage());
+            } catch (Exception exc){
+                System.out.println("Other exception: "+ exc.getMessage());
+                exc.printStackTrace();
+            }
+        } else if(args.length == 0){
+            System.out.println("No given file");
+        } else {
+            System.out.println("Too many arguments");
+        }
+    }
+}
--- src/com/ParserMain.java
+++ src/com/ParserMain.java
@@ -1,26 +1,23 @@
 package com;
 
 import com.byteReader.StreamReader;
+import com.parseException.ParseException;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-public class Main {
-
+public class ParserMain {
     public static void main(String[] args) {
         if(args.length == 1){
             try {
                 FileInputStream inputStream = new FileInputStream(args[0]);
-                Lexer lexer = new Lexer(new StreamReader(inputStream));
-                //Lexer operations
-                System.out.println("start");
-                while(lexer.getToken().getType() != Token.Type.end_of_bytes_){
-                    Token token = lexer.readNextToken();
-                    System.out.println(token.getPosition().sign + "\t" + token.getPosition().line + "\t" + Token.tokenNames.get(token.getType()) + ":\t\t" +  token.getValue());
-                }
+                Parser parser = new Parser(new StreamReader(inputStream));
+                parser.parse();
                 System.out.println("end");
             } catch (FileNotFoundException exc){
                 System.out.println("No such file or invalid argument: " + exc.getMessage());
+            } catch (ParseException exc) {
+                System.out.println(exc.getMessage());
             } catch (Exception exc){
                 System.out.println("Other exception: "+ exc.getMessage());
                 exc.printStackTrace();
